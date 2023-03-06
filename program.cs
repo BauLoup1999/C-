348d65b70976b943f6a2d7ae52ae2a7fd99c40d0
Loup <loupbauduin@gmail.com>
2023-03-06 11:19:44 +0100

    cs

---
+++ program.cs
@@ -0,0 +1,22 @@
+class Program
+(
+    static vois Square(int a, int b)
+(
+    a = a + 1;
+    b++;
+    a = a * a;
+    b *= b;
+    Console.WriteLine(a + " " + b);
+)
+
+static void Main(string[] arge)
+(
+    int num1 = 5;
+    int num2 = 10;
+    Console.WriteLine(num1 + " " + num2)
+    Square(num1, num2);
+    Console.WriteLine(num1 + " " + num2);
+    Console.WriteLine("Press Enter Key to Exit...");
+    Console.ReadLine();
+)
+)
